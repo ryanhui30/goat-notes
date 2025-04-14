2d49777a0534bec1efa60c4ddaeefbfc6c42261b
Ryan Hui <ryanhui30@gmail.com>
2025-04-14 14:37:05 -0700

    Add delete notes button, select notes button progress.

---
+++ src/components/DeleteNoteButton.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+type Props = {
+  noteId: string;
+  deleteNoteLocally: (noteId: string) => void;
+};
+
+function DeleteNoteButton({ noteId, deleteNoteLocally }: Props) {
+  return <div>DeleteNoteButton</div>
+}
+
+export default DeleteNoteButton;
